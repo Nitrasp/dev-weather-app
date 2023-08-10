e949a30425cd8160d6c882053be8d7441fef348e
Kosuke Tanaka <tanaka.kos02@gmail.com>
2023-08-10 23:50:23 +0900

    完成-APIの結果を表示するHTMLの作成

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'weather',
+    'config',
 ]
 
 MIDDLEWARE = [
@@ -115,11 +117,17 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIR = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = [
+    ('css', os.path.join(BASE_DIR, 'weather/static/css')),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+import requests
+from django.http import JsonResponse
+from config.settings import API_KEY
 
-# Create your views here.
-
+# 初期表示ビュークラス
 class InitView(TemplateView):
     
     template_name = 'index.html'
+    
+    # 変数を渡す
+    def get_context_data(self,**kwargs):
+        
+        context = super().get_context_data(**kwargs)
+        
+        location = 'Tokyo,JP'
+        
+        # 都市の気象情報を取得
+        weather_info = self.weather_api_get(location)
+        
+        # コンテキストに情報を設定
+        context['weather'] = weather_info
+        
+        return context
+    
+    # APIリクエストの発行
+    def weather_api_get(self, city):
+        
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=ja&units=metric'
+        response = requests.get(url)
+        data = response.json()  # レスポンスをJSON形式で取得
+        
+        return data
+    
