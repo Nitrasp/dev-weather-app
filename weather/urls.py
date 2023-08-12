bd35e39e24ab2c43572efd29cdc9d778cad444f2
Kosuke Tanaka <tanaka.kos02@gmail.com>
2023-08-12 21:56:49 +0900

    週間天気の実装（途中

--- weather/urls.py
+++ weather/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('daily/', views.InitView.as_view(), name='daily'),
-    path('daily/<str:country>/', views.InitView.as_view(), name='daily'),
+    path('daily/<str:city>/', views.InitView.as_view(), name='daily'),
     path('weekly/', views.InitView.as_view(), name='weekly'),
-    path('weekly/<str:country>/', views.InitView.as_view(), name='weekly'),
+    path('weekly/<str:city>/', views.WeeklyView.as_view(), name='weekly'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from enum import Enum
 from .models import City
-from . import util
+from . import Utils
 import datetime, locale
 
 # 共通処理クラス
@@ -24,26 +24,53 @@ class baseView(TemplateView):
 class InitView(baseView):
         
     # GET通信
-    def get(self, request, country=None):
+    def get(self, request, city=None):
         
         template_name = 'index.html'
         
-        if country == None:
-            location = f'Tokyo,{COUNTRY_NAME}'
-        else:
-            location = f'{country},{COUNTRY_NAME}'
-        
         # 都市の気象情報を取得
-        weather_info = util.weather_api_get(location)
+        weather_info = Utils.weather_api_get(city, Utils.Api_type.Daily)
         
         # localeモジュールで時間のロケールを'ja_JP.UTF-8'に変更する
         locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
+        # UNIX時間の変換
         dt_text = datetime.datetime.fromtimestamp(weather_info['dt']).strftime ( '%m月 %d日(%a)' )
-        print(dt_text)
         
         # コンテキストに情報を設定
         context = super().get_context_data()
         context['weather'] = weather_info
         context['dt_text'] = dt_text
+
+        return render(request, template_name, context)
+    
+class WeeklyView(baseView):
+    # GET通信
+    def get(self, request, city=None):
+        
+        template_name = 'weekly.html'
+        
+        # 都市の気象情報を取得
+        response_api_list = Utils.weather_api_get(city, Utils.Api_type.Weekly)
+        
+        # 都市名の取得
+        city_name = response_api_list['city']['name']
+        
+        # localeモジュールで時間のロケールを'ja_JP.UTF-8'に変更する
+        locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
+        
+        # 時間毎の気象情報リストを作成
+        list = []
+        for res in response_api_list['list']:
+            # UNIX時間の変換
+            dt_text = datetime.datetime.fromtimestamp(res['dt']).strftime('%m月 %d日(%a)')
+            # 月日(E)、各気象情報辞書の作成
+            list.append(dict(dt_text=dt_text, main=res['main'], weather=res['weather'], wind=res['wind']))
+        
+        # テンプレート用のコンテキスト作成
+        context = super().get_context_data()
+        # 都市名を格納
+        context['city_name'] = city_name
+        # 時間毎の気象情報辞書を格納
+        context['list'] = list
 
         return render(request, template_name, context)
